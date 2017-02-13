14efde49cab7df8dd13118ee529211972076628c
xuhui <good122000@qq.com>
2017-02-13 23:23:11 +0800

    misc

---
+++ DynamOCTest/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  DynamOCTest
+//
+//  Created by Xuhui on 13/02/2017.
+//  Copyright © 2017 hui xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ DynamOCTest/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  DynamOCTest
+//
+//  Created by Xuhui on 13/02/2017.
+//  Copyright © 2017 hui xu. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
