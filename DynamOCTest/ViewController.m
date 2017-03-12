d93f3545842ce15221c0b2296cb752bf2086881e
xuhui <good122000@qq.com>
2017-03-12 23:57:08 +0800

    misc

--- DynamOCTest/ViewController.m
+++ DynamOCTest/ViewController.m
@@ -11,7 +11,9 @@
 #import <LuaContext.h>
 #import <DynamBlock.h>
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
 
 @end
 
@@ -19,15 +21,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     LuaContext *context = get_current_luacontext();
     NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"lua"];
     [context evaluateScript:[NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil]];
-    NSLog(@"xxxxx");
-    //TestClass *test = [[TestClass alloc] init];
-    //[test performSelector:@selector(echo:) withObject:@1];
-    //Class helloClass = NSClassFromString(@"HelloClass");
-    //id test = [[helloClass alloc] init];
-//    NSLog(@"%@", [test performSelector:@selector(str) withObject:nil]);
+   
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 }
 
 
@@ -36,5 +36,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UITableViewDelegate
+
+#pragma mark - UITableViewDataSource
 
 @end
